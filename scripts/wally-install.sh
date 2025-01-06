0b071f60412b090a3106320a2b58ad4c723417f4
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2025-01-06 01:04:57 -0700

    initial commit

--- scripts/wally-install.sh
+++ scripts/wally-install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+set -e
 ROJO_PROJECT="$1"
 wally install
+# if "Packages" doesn't exist, create it
+if [ ! -d "Packages" ]; then
+  mkdir Packages
+fi
 rojo sourcemap "$ROJO_PROJECT" --output sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages
+
